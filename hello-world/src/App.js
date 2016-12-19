ca6394a814c379432bfd5911ef541e81ea3b783a
BindiChen <bindi.chen@hotmail.com>
2016-12-19 00:17:16 +0000

    Access nested elements with React props.children

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            text: 'This is state text',
-            cat: 0
-        };
-    }
-
-    update(e) {
-        this.setState({text: e.target.value});
-    }
-
     render() {
         return (
-            <div>
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
-                <h1>{this.state.text} - {this.state.cat}</h1>
-            </div>    
+            <Button>
+                <Heart />
+                React
+            </Button>
         );
     }
 }
 
-const Widget = (props) => 
-    <input type="text" onChange={props.update} />
+// Access nested elements with React props.children
+const Button = (props) => <button>{props.children}</button>
 
+class Heart extends React.Component {
+    render() {
+        return (<span>&hearts;</span>)
+    }
+}
 
 export default App;
