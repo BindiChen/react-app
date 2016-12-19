a0039aa92ce21715183d4a5118fa11cd3cb2146c
BindiChen <bindi.chen@hotmail.com>
2016-12-19 00:04:05 +0000

    React component children

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -16,12 +16,17 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <input type="text" onChange={this.update.bind(this)}/>
+                <Widget update={this.update.bind(this)} />
+                <Widget update={this.update.bind(this)} />
+                <Widget update={this.update.bind(this)} />
                 <h1>{this.state.text} - {this.state.cat}</h1>
             </div>    
         );
     }
 }
+
+const Widget = (props) => 
+    <input type="text" onChange={props.update} />
 
 
 export default App;
