653103bff9d1e0b4a678bb7e37a9620d9e70ac9f
BindiChen <bindi.chen@hotmail.com>
2016-12-19 00:39:01 +0000

    Custom propType validation

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,21 +3,24 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <Button>
-                <Heart />
-                React
-            </Button>
+            <Title text="12345"></Title>
         );
     }
 }
 
-// Access nested elements with React props.children
-const Button = (props) => <button>{props.children}</button>
 
-class Heart extends React.Component {
-    render() {
-        return (<span>&hearts;</span>)
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+// Custom propType validation
+Title.propTypes = {
+    text(props, propName, component) {
+        if(!(propName in props)) {
+            return new Error(`mising ${propName}`);
+        }
+        if(props[propName].length < 6) {
+            return new Error(`${propName} was too short`);
+        }
     }
-}
+};
 
 export default App;
