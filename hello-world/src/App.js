290b1988ce21af53cc547297dbd0d8053801be1c
BindiChen <bindi.chen@hotmail.com>
2016-12-30 21:44:41 +0000

    component updates when new props are received

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,56 +3,40 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-   constructor(){
-       super();
-       this.state = {val: 0};
-       this.update = this.update.bind(this);
-   }
-
-   update(){
-       this.setState({val: this.state.val + 1});
-   }
-
-   componentWillMount(){
-       console.log('componentWillMount');
-       this.setState({m:2});
-   }
-
-   componentDidMount(){
-       console.log('componentDidMount');
-       this.inc = setInterval(this.update, 500);
-   }
-
-   componentWillUnmount(){
-       console.log('compoenntWillUnmount');
-       // display the mounted dom
-       // console.log(ReactDOM.findDOMNode(this));
-       clearInterval(this.inc);
-   }
-
-   render(){
-       console.log('render');
-       return <button onClick={this.update}>{this.state.val*this.state.m} </button>
-   }
-}
-
-class Wrapper extends React.Component {
-    mount(){
-        ReactDOM.render(<App />, document.getElementById('a'))
+    constructor() {
+        super();
+        this.state = {increasing: false};
     }
-    unmount(){
-        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+
+    update() {
+        ReactDOM.render(
+            <App val={this.props.val+1} />,
+            document.getElementById('root')
+        )
     }
-    render(){
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({increasing: nextProps.val > this.props.val})
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps.val % 5 === 0;
+    // }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(`prevProps: ${prevProps.val}`)
+    }
+
+    render() {
+        console.log(this.state.increasing);
         return (
-            <div>
-                <button onClick={this.mount.bind(this)}>Mount</button>
-                <button onClick={this.unmount.bind(this)}>Unmount</button>
-                <div id="a"></div>
-            </div>
+             <button onClick={this.update.bind(this)}>
+                 {this.props.val}
+             </button>
         );
     }
 }
 
+App.defaultProps = {val: 0};
 
-export default Wrapper;
+export default App;
